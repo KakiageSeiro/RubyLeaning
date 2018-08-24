b8743248afafd35ea7cced03d161442a3445ef74
YutaEbisuno <frederika07_bernkastel@yahoo.co.jp>
2018-08-25 01:34:58 +0900

    単位（メートル、フィート、インチ）の変換クラスを実装

---
+++ ruby-book/test/convert_length_test.rb
@@ -0,0 +1,13 @@
+# テスト
+require 'minitest/autorun'
+
+# require を利用する場合（ruby実行フォルダを起点）
+require './lib/convert_length'
+
+class ConvertLengthTest < Minitest::Test
+  def test_convert_length
+    assert_equal 39.37, convert_length(1, form: :m, to: :in)
+    assert_equal 0.38, convert_length(15, form: :in, to: :m)
+    assert_equal 10670.73, convert_length(35000, form: :ft, to: :m)
+  end
+end
